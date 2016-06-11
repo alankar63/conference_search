2034245c0ca7417bc8272fa48242ded9ca264fc6
alankar63 <alankar63@gmail.com>
2016-06-11 20:01:02 +0530

    name fixes

--- video_search/video_search/settings.py
+++ video_search/video_search/settings.py
@@ -76,14 +76,6 @@ WSGI_APPLICATION = 'video_search.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'video_db',                      
--- video_search/vstream/video_search.py
+++ video_search/vstream/video_search.py
@@ -1,13 +1,12 @@
 import json
 import re
 import urllib
-from BeautifulSoup import BeautifulSoup
 from mechanize import Browser
 
-START_LINK = "https://api.import.io/store/connector/_magic?url=https%3A%2F%2F"
-END_LINK = "%2F&format=JSON&js=false&_apikey=aac429e34e5c49b2b52a5c3a9303febe084eb7959f\
-63562017b1cc159a6e1e2565f371008ffe01ff9422d76097c4acf642cc7a4d28615fffc992d17fc737059d4\
-b122b9b163ee36c0192e10c29b14c3a"
+PREFIX_LINK = "https://api.import.io/store/connector/_magic?url=https%3A%2F%2F"
+SUFFIX_LINK = "%2F&format=JSON&js=false&_apikey=aac429e34e5c49b2b52a5c3a9303febe084eb79\
+59f63562017b1cc159a6e1e2565f371008ffe01ff9422d76097c4acf642cc7a4d28615fffc992d17fc73705\
+9d4b122b9b163ee36c0192e10c29b14c3a"
 
 # test links .Remove them later
 air_mozilla_2015_engineering = "https://air.mozilla.org/channels/engineering/"
@@ -19,7 +18,7 @@ pycon_2014 = "https://www.youtube.com/user/PyCon2014"
 def create_link(link):
     link = link.replace("https://", "")
     link = link.replace("/", "%2F")
-    complete_link = START_LINK + link + END_LINK
+    complete_link = PREFIX_LINK + link + SUFFIX_LINK
     return complete_link
 
 
