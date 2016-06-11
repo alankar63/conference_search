b2c9c65e818078029cf7c4728df44f61644f9f2b
alankar63 <alankar63@gmail.com>
2016-06-11 19:44:38 +0530

    slight fixes

--- video_search/vstream/apps.py
+++ video_search/vstream/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class Video1Config(AppConfig):
-    name = 'video1'
+class vstreamConfig(AppConfig):
+    name = 'vstream'
