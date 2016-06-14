b5653fcea6950628f3c822fac4863444df2e4060
alankar63 <alankar63@gmail.com>
2016-06-14 09:02:03 +0530

    management command added

--- video_search/video_search/settings.py
+++ video_search/video_search/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'videodata',                      
             'USER': 'alankar63',
-            'PASSWORD': 'juanmata',
+            'PASSWORD': '123456123456',
             'HOST': 'localhost',
             'PORT': '',
                                                               }
--- video_search/vstream/video_search.py
+++ video_search/vstream/video_search.py
@@ -2,13 +2,14 @@ import json
 import re
 import urllib
 from mechanize import Browser
+from vstream.models import Videoinfo
 
 PREFIX_LINK = "https://api.import.io/store/connector/_magic?url=https%3A%2F%2F"
 SUFFIX_LINK = "%2F&format=JSON&js=false&_apikey=aac429e34e5c49b2b52a5c3a9303febe084eb79\
 59f63562017b1cc159a6e1e2565f371008ffe01ff9422d76097c4acf642cc7a4d28615fffc992d17fc73705\
 9d4b122b9b163ee36c0192e10c29b14c3a"
 
-# test links .Remove them later
+# test links. Remove them later
 air_mozilla_2015_engineering = "https://air.mozilla.org/channels/engineering/"
 pycon_india_2015 = "https://www.youtube.com/channel/UCgxzjK6GuOHVKR_08TT4hJQ/videos?spf\
 reload=5"
@@ -29,10 +30,15 @@ def collect_info_mozilla(mozilla_link):
     for data in video_info.get("tables"):
         results = data.get("results")
         for data in results:
-            print data.get("entrysummary_link/_source")  # name
-            print data.get("entrysummary_link")  # link
-            print data.get("entrysummary_description")  # description
-            print data.get("timeago_value")  # date and time
+            name = data.get("entrysummary_link/_source")  # name
+            link = data.get("entrysummary_link")  # link
+            description = data.get("entrysummary_description")  # description
+            videodata = data.get("timeago_value")  # date
+            new_data, created = Videoinfo.objects.get_or_create(name=name,
+                                    url=link,
+                                    description=description,
+                                    videodate=videodate)
+            new_data.save()
 
 
 def collect_info_youtube(youtube_channel_link):
@@ -42,18 +48,19 @@ def collect_info_youtube(youtube_channel_link):
     for data in video_info.get("tables"):
         results = data.get("results")
         for data in results:
-            print data.get("uixtile_link/_text")  # name
-            print data.get("uixtile_link")  # link
-            print data.get("lockupmeta_value_numbers")  # number of views.
-            print data.get("videotime_value")  # duration 
+            name = data.get("uixtile_link/_text")  # name
+            link = data.get("uixtile_link")  # link
+            print data.get("videotime_value")  # duration
+            new_data, created = Videoinfo.objects.get_or_create(name=name,
+                                    url=link)
+            new_data.save()
+            print new_data.name  # remove it later
 
 
 def input_video_link():
+    print "channel link"
     link = raw_input()
     if "youtube" in link:
         collect_info_youtube(link)
     elif "mozilla" in link:
         collect_info_mozilla(link)
-
-input_video_link()
-
